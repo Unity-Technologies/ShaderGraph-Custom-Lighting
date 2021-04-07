0686b3beafc9ebd326436e478e996d66382cf1e7
Felix Herbst <felix.herbst@gmail.com>
2021-04-07 11:23:11 +0200

    fixed SHADERGRAPH_PREVIEW if/ifdef

--- Assets/Includes/CustomLighting.hlsl
+++ Assets/Includes/CustomLighting.hlsl
@@ -3,13 +3,13 @@
 
 void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Direction = float3(0.5, 0.5, 0);
     Color = 1;
     DistanceAtten = 1;
     ShadowAtten = 1;
 #else
-#if SHADOWS_SCREEN
+#ifdef SHADOWS_SCREEN
     float4 clipPos = TransformWorldToHClip(WorldPos);
     float4 shadowCoord = ComputeScreenPos(clipPos);
 #else
@@ -25,13 +25,13 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
 
 void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Direction = half3(0.5, 0.5, 0);
     Color = 1;
     DistanceAtten = 1;
     ShadowAtten = 1;
 #else
-#if SHADOWS_SCREEN
+#ifdef SHADOWS_SCREEN
     half4 clipPos = TransformWorldToHClip(WorldPos);
     half4 shadowCoord = ComputeScreenPos(clipPos);
 #else
@@ -47,7 +47,7 @@ void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out h
 
 void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, float3 Color, float3 WorldNormal, float3 WorldView, out float3 Out)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Out = 0;
 #else
     Smoothness = exp2(10 * Smoothness + 1);
@@ -59,7 +59,7 @@ void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, f
 
 void DirectSpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out)
 {
-#if SHADERGRAPH_PREVIEW
+#ifdef SHADERGRAPH_PREVIEW
     Out = 0;
 #else
     Smoothness = exp2(10 * Smoothness + 1);
